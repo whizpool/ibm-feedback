9eba0962a0a1fa566bd0eba8ac1a121bd72b779c
Muhammad Naeem <muhammad.naeem@whizpool.com>
2020-12-24 12:47:04 +0500

    Version 0.2 updated missing files in last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component, } from 'react';
+import React, { Component} from 'react';
 //import logo from './logo.svg';
 import './app.scss';
 import { Content,HeaderContainer } from 'carbon-components-react';
 //import { Route, Switch, Link } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
+import { connect} from 'react-redux'
 
 
 import AppHeader from './components/AppHeader';
@@ -12,26 +13,62 @@ import AdminPage from './content/AdminPage';
 import FeedbackPage from './content/FeedbackPage';
 import ViewPage from './content/ViewPage';
 import EditPage from './content/EditPage';
-import RepoPage from './content/RepoPage';
+import LoginPage from './content/LoginPage';
 
 
+const mapStateToProps = (state) => {
+
+	return {
+			isLogged: state.auth.isLogged,
+			account_id: state.auth.account_id, 
+			email: state.auth.email, 
+			name: state.auth.name, 
+			role: state.auth.role
+			
+		};
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        saveLogoutState: (data) => dispatch(data),
+    }
+}	
+	
 class App extends Component {
+	
+	
+	onClickSignOut  = (event) => {	
+			event.preventDefault();
+			this.props.saveLogoutState({type: 'SIGN_OUT'})
+	};
+	
+	componentDidMount() {
+    document.title = 'React Login';
+  }
+	
   render() {
 	   return (
 			<div className="container">
 			<HeaderContainer
 			render={({ isSideNavExpanded, onClickSideNavExpand }) => (
 				<>
-					<AppHeader isSideNavExpanded={isSideNavExpanded} onClickSideNavExpand={onClickSideNavExpand} />
-					<Content className="bx--offset-lg-2">
+					{this.props.isLogged? <AppHeader isSideNavExpanded={isSideNavExpanded} onClickSideNavExpand={onClickSideNavExpand} onClickSignOut={this.onClickSignOut}  userData = {this.props} /> : "" }
+					<Content className={this.props.isLogged? "bx--offset-lg-2" : "fullpage" }>
 						<Switch>
-							<Route exact path="/" component={WidgetPage} />							
-							<Route path="/feedbacks" component={FeedbackPage} />
-							<Route path="/admins" component={AdminPage} />
-							<Route path="/view/:id" component={ViewPage} />
-							<Route path="/edit/:id/:configure" component={EditPage} />
+							<Route path="/login" component={LoginPage} />
 							
-							<Route path="/repos" component={RepoPage} />
+							{
+								this.props.isLogged ?
+								<>
+								<Route exact path="/" component={WidgetPage} />							
+								<Route path="/feedbacks" component={FeedbackPage} />
+								<Route path="/admins" component={AdminPage} />
+								<Route path="/view/:id" component={ViewPage} />
+								<Route path="/edit/:id/:configure" component={EditPage} />
+								</>
+								:
+									<Redirect to="/login"/>
+							}
+							
 						</Switch>
 					</Content>
 				</>
@@ -42,4 +79,5 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter  as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux'
+
+import store from './store'
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+	<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+	</Provider>,
   document.getElementById('root')
 );
 
