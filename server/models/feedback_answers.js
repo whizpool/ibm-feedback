77b618e826739509ca89526da03616063a124380
Muhammad Naeem <muhammad.naeem@whizpool.com>
2020-12-29 10:19:33 +0500

    Model Updated

--- server/models/feedback_answers.js
+++ server/models/feedback_answers.js
@@ -31,7 +31,7 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: false
 	  },
 	  answer: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false
 	  },
   }, {
