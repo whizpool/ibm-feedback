2ddb3ace097784c8f763dc36d2a07d188c174bde
Muhammad Naeem <muhammad.naeem@whizpool.com>
2021-01-27 17:14:58 +0500

    Update

--- server/config/config.global.js
+++ server/config/config.global.js
@@ -3,13 +3,20 @@ var config = module.exports = {};
 config.env = 'undefined';
 config.hostname = 'undefined.example.com';
 config.sessionSecret = 'undefined-session-secret';
-config.debug = true;
+config.debug = false;
+/*FRONT END PUBLIC ASSET*/
 config.ratingImageUrl = 'http://localhost:3000/assets/';
+
+/*API END HOST*/
 config.apihost = 'http://localhost:8080/api/v1/';
+
+/*IBM Service credentials Please here*/
+/*Copy the IBM Cloud storage bucket name and write in the BucketName param*/
 config.IBMCONFIG = {
   useHmac: true,
 	bucketName: 'cloud-object-xxxxxxxxxxxxx',
 	serviceCredential: [Service credentials-with-hmac goes here],
 };
+/*S3 API*/
 config.defaultEndpoint = 's3.us.cloud-object-storage.appdomain.cloud';
 
